950e19a404de002e3c2dcccc16679ad750bacd2a
md Yasin <mdyasin48902@gmail.com>
2025-06-05 14:51:21 +0600

    added dainamice route for home details page

--- index.js
+++ index.js
@@ -26,6 +26,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+// Legacy-Vault  Artifacts
+const Artifactscollection = client.db('Legacy-Vault').collection('Artifacts');
+
+app.get('/artifacts' ,async (req,res)=>{
+    const cursor = Artifactscollection.find();
+    const result = await cursor.toArray();
+    res.send(result)
+})
+
+const { ObjectId } = require('mongodb'); // খুব গুরুত্বপূর্ণ!
+
+app.get('/artifacts/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const artifact = await Artifactscollection.findOne(query);
+  res.send(artifact);
+});
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
