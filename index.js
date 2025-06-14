bdab0727c902e68ffc5e2e87a5d318d9b2793761
md Yasin <mdyasin48902@gmail.com>
2025-06-14 17:43:16 +0600

    deployed in vercel

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app =express();
-const jwt = require('jsonwebtoken');
-const cookieparser = require('cookie-parser');
-const port = process.env.PORT || 3000 ;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const { ObjectId } = require('mongodb');
-app.use(cors({
-  origin:['http://localhost:5173'],
-  credentials:true
-
-}));
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const jwt = require("jsonwebtoken");
+const cookieparser = require("cookie-parser");
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { ObjectId } = require("mongodb");
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://assignment-11-1588b.web.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieparser());
 
-const verifyuser = (req,res,next)=>{
-  const token =req?.cookies?.token;
-  console.log('cookie in the middlewire', token);
-  if(!token){
-    return res.status(401).send({massage:'unauthorized access'})
+const verifyuser = (req, res, next) => {
+  const token = req?.cookies?.token;
+  console.log("cookie in the middlewire", token);
+  if (!token) {
+    return res.status(401).send({ massage: "unauthorized access" });
   }
-jwt.verify(token,process.env.jwt_access_secret,(err,decoded)=>{
-  if(err){
-    return res.status(401).send({massage:'unauthorized access'})
-  }
-  req.decoded=decoded;
-  next();
-} )
+  jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ massage: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
-
-
-}
-
-
-const uri = `mongodb+srv://${process.env.user}:${process.env.db_pass}@cluster0.vbsgl0h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.USER}:${process.env.DB_PASS}@cluster0.vbsgl0h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -42,47 +39,58 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-// Legacy-Vault  Artifacts
-const Artifactscollection1 = client.db('Legacy-Vault').collection('Artifacts');
-const Artifactscollection2 = client.db('Legacy-Vault').collection('Historical Timeline');
+    // await client.connect();
+    // Legacy-Vault  Artifacts
+    const Artifactscollection1 = client
+      .db("Legacy-Vault")
+      .collection("Artifacts");
+    const Artifactscollection2 = client
+      .db("Legacy-Vault")
+      .collection("Historical Timeline");
 
-// jwt api
+    // jwt api
 
-app.post('/jwt', async(req, res)=>{
-  const {email} = req.body;
-const user = {email}
-const token = jwt.sign(user,process.env.jwt_access_secret,{expiresIn:'1h'});
+    app.post("/jwt", async (req, res) => {
+      const { email } = req.body;
+      const user = { email };
+      const token = jwt.sign(user, process.env.JWT_ACCESS_SECRET, {
+        expiresIn: "1h",
+      });
 
-res.cookie('token',token,{
-  httpOnly:true,
-  secure:false,
-})
+      res.cookie("token", token, {
+        // httpOnly:true,
+        // // secure:false,
+        // secure: process.env.NODE_ENV === "production",
+        // sameSite:'None'
 
-res.send({success:true})
+        // httpOnly: true,
+        // secure: process.env.NODE_ENV === "production",
+        // sameSite: process.env.NODE_ENV === "production" ? 'None' : 'Lax',
 
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      });
 
-})
+      res.send({ success: true });
+    });
 
+    // Artifacts form
 
-
-// Artifacts form
-
-app.post("/addartifacts",verifyuser, async (req, res) => {
+    app.post("/addartifacts", verifyuser, async (req, res) => {
       try {
         const newArtifact = req.body;
-        const email=req.body.email;
+        const email = req.body.email;
 
-        
-if(email !== req.decoded.email){
-      return res.status(403).send({massage:'forbidden access'})
-    }
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ massage: "forbidden access" });
+        }
         const result = await Artifactscollection1.insertOne(newArtifact);
         res.status(201).json({
           success: true,
@@ -98,170 +106,158 @@ if(email !== req.decoded.email){
       }
     });
 
+    // delete operation for artifacts
+    app.delete("/delete/:id", verifyuser, async (req, res) => {
+      try {
+        const email = req.decoded.email;
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ massage: "forbidden access" });
+        }
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await Artifactscollection1.deleteOne(query);
 
-// delete operation for artifacts
-app.delete('/delete/:id', verifyuser,async (req, res) => {
-  try {
-    
-const email=req.decoded.email;
-if(email !== req.decoded.email){
-      return res.status(403).send({massage:'forbidden access'})
-    }
-    const id = req.params.id;
-    const query = { _id: new ObjectId(id) };
-    const result = await Artifactscollection1.deleteOne(query);
-
-    if (result.deletedCount === 1) {
-      res.status(200).json({
-        success: true,
-        message: 'Artifact deleted successfully',
-      });
-    } else {
-      res.status(404).json({
-        success: false,
-        message: 'Artifact not found',
-      });
-    }
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: 'Failed to delete artifact',
-      error: error.message,
+        if (result.deletedCount === 1) {
+          res.status(200).json({
+            success: true,
+            message: "Artifact deleted successfully",
+          });
+        } else {
+          res.status(404).json({
+            success: false,
+            message: "Artifact not found",
+          });
+        }
+      } catch (error) {
+        res.status(500).json({
+          success: false,
+          message: "Failed to delete artifact",
+          error: error.message,
+        });
+      }
     });
-  }
-});
 
+    app.get("/historical_timeline", async (req, res) => {
+      const cursor = Artifactscollection2.find();
+      console.log("inside timline", req.cookies);
 
-app.get('/historical_timeline' ,verifyuser,async (req,res)=>{
-    const cursor = Artifactscollection2.find();
-    console.log('inside timline',req.cookies);
-    const email=req.decoded.email;
-
-    if(email !== req.decoded.email){
-      return res.status(403).send({massage:'forbidden access'})
-    }
-    const result = await cursor.toArray();
-    res.send(result)
-})
-
-
-app.get('/artifacts' ,verifyuser ,async (req,res)=>{
-  
-    const cursor = Artifactscollection1.find();
-    const result = await cursor.toArray();
-    res.send(result)
-})
-
-// like oparetion start
-
-app.patch('/artifacts/like/:id',verifyuser, async (req, res) => {
-  const artifactId = req.params.id;
-  const userEmail = req.body.email;
-  
-if(userEmail !== req.decoded.email){
-      return res.status(403).send({massage:'forbidden access'})
-    }
-
-  if (!userEmail) {
-    return res.status(400).send({ message: 'Email is required' });
-  }
-
-  const query = { _id: new ObjectId(artifactId) };
-  const artifact = await Artifactscollection1.findOne(query);
-
-  if (!artifact) {
-    return res.status(404).send({ message: 'Artifact not found' });
-  }
-
-  const alreadyLiked = artifact.liked_by?.includes(userEmail);
-
-  let updateDoc;
-  if (alreadyLiked) {
-    // Remove like
-    updateDoc = {
-      $pull: { liked_by: userEmail }
-    };
-  } else {
-    // Add like
-    updateDoc = {
-      $addToSet: { liked_by: userEmail }
-    };
-  }
-
-  const result = await Artifactscollection1.updateOne(query, updateDoc);
-  res.send(result);
-});
-
-
-
-// like oparetion end
-
-// for update form show data 
-
-app.get('/update/:id',verifyuser , async (req, res) => {
-  const email=req.decoded.email;
-  if(email !== req.decoded.email){
-      return res.status(403).send({massage:'forbidden access'})
-    }
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  const artifact = await Artifactscollection1.findOne(query);
-  res.send(artifact);
-});
-
-//  update form for update data 
-
-
-app.patch('/update/:id',verifyuser , async (req, res) => {
-  try {
-    const email=req.body.email;
-    if(email !== req.decoded.email){
-      return res.status(403).send({massage:'forbidden access'})
-    }
-    const id = req.params.id;
-    const updatedData = req.body;
-
-    const filter = { _id: new ObjectId(id) };
-    const updateDoc = {
-      $set: updatedData,
-    };
-
-    const result = await Artifactscollection1.updateOne(filter, updateDoc);
-
-    if (result.matchedCount === 0) {
-      return res.status(404).json({
-        success: false,
-        message: 'Artifact not found',
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      message: 'Artifact updated successfully',
-      modifiedCount: result.modifiedCount,
+      const result = await cursor.toArray();
+      res.send(result);
     });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: 'Failed to update artifact',
-      error: error.message,
+
+    app.get("/artifacts", async (req, res) => {
+      const cursor = Artifactscollection1.find();
+      const result = await cursor.toArray();
+      res.send(result);
     });
-  }
-});
 
- 
-//for  home details
-app.get('/artifacts/:id',verifyuser ,async (req, res) => {
+    // like oparetion start
 
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  const artifact = await Artifactscollection1.findOne(query);
-  res.send(artifact);
-});
+    app.patch("/artifacts/like/:id", verifyuser, async (req, res) => {
+      const artifactId = req.params.id;
+      const userEmail = req.body.email;
+
+      if (userEmail !== req.decoded.email) {
+        return res.status(403).send({ massage: "forbidden access" });
+      }
+
+      if (!userEmail) {
+        return res.status(400).send({ message: "Email is required" });
+      }
+
+      const query = { _id: new ObjectId(artifactId) };
+      const artifact = await Artifactscollection1.findOne(query);
+
+      if (!artifact) {
+        return res.status(404).send({ message: "Artifact not found" });
+      }
+
+      const alreadyLiked = artifact.liked_by?.includes(userEmail);
+
+      let updateDoc;
+      if (alreadyLiked) {
+        // Remove like
+        updateDoc = {
+          $pull: { liked_by: userEmail },
+        };
+      } else {
+        // Add like
+        updateDoc = {
+          $addToSet: { liked_by: userEmail },
+        };
+      }
+
+      const result = await Artifactscollection1.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    // like oparetion end
+
+    // for update form show data
+
+    app.get("/update/:id", verifyuser, async (req, res) => {
+      const email = req.decoded.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ massage: "forbidden access" });
+      }
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const artifact = await Artifactscollection1.findOne(query);
+      res.send(artifact);
+    });
+
+    //  update form for update data
+
+    app.patch("/update/:id", verifyuser, async (req, res) => {
+      try {
+        const email = req.body.email;
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ massage: "forbidden access" });
+        }
+        const id = req.params.id;
+        const updatedData = req.body;
+
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: updatedData,
+        };
+
+        const result = await Artifactscollection1.updateOne(filter, updateDoc);
+
+        if (result.matchedCount === 0) {
+          return res.status(404).json({
+            success: false,
+            message: "Artifact not found",
+          });
+        }
+
+        res.status(200).json({
+          success: true,
+          message: "Artifact updated successfully",
+          modifiedCount: result.modifiedCount,
+        });
+      } catch (error) {
+        res.status(500).json({
+          success: false,
+          message: "Failed to update artifact",
+          error: error.message,
+        });
+      }
+    });
+
+    //for  home details
+    app.get("/artifacts/:id", verifyuser, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const artifact = await Artifactscollection1.findOne(query);
+      res.send(artifact);
+    });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -269,10 +265,9 @@ app.get('/artifacts/:id',verifyuser ,async (req, res) => {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req,res) =>{
-    res.send('Historical Artifacts server')
+app.get("/", (req, res) => {
+  res.send("Historical Artifacts server");
 });
- app.listen(port ,()=>{
-    console.log(`Historical Artifacts server on port ${port}`);
- });
+app.listen(port, () => {
+  console.log(`Historical Artifacts server on port ${port}`);
+});
